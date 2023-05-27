f35380c4714bfbd1dd35d00819780bc0f213146a
samd1904 <samd1904@gmail.com>
2023-05-27 20:08:20 +0530

    Got dice roll command working

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-// Require the necessary discord.js classes
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { pingCommand, execute } from "./commands/ping.js";
 import dotenv from "dotenv";
 dotenv.config();
 // Create a new client instance
@@ -14,10 +11,40 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on("interactionCreate", async interaction => {
-    if(!interaction.isChatInputCommand()) return;
-
-    if(interaction.commandName === 'ping'){
-        await execute(interaction);
+    try{
+        if(!interaction.isChatInputCommand()) return;
+        //replace if statements with switch case
+        console.log("Command: ",interaction.commandName)
+        switch (interaction.commandName) {
+            case 'ping':
+                await interaction.reply("Pong!");
+                break;
+            case 'roll':
+                if(interaction.options.getString('dice_expression').includes('d')){
+                    const optionStrArr = interaction.options.getString('dice_expression').split('d');
+                    const dCount = optionStrArr[0];
+                    const dice = parseInt(optionStrArr[1]);
+                    const maxRoll = parseInt(optionStrArr[1])*dCount
+                    let i = 0;
+                    let rolls = [];
+                    let rollTotal = 0;
+                    while(i < dCount){
+                        let roll = Math.round(Math.random()*(dice-1+1)+1);
+                        rollTotal += roll;
+                        rolls.push(roll);
+                        ++i;
+                    }
+                    await interaction.reply(`${interaction.member.user.username} rolled ${rolls}. The total is ${rollTotal} out of ${maxRoll}`);
+                    return;
+                }else{
+                    await interaction.reply(`Format is correct, please pass the dice expression in NdM format (eg: 2d8)`);
+                    return;
+                }
+                break;
+        }
+    }catch(error){
+        console.error(error.message);
+        return;
     }
 });
 
--- register.js
+++ register.js
@@ -2,20 +2,32 @@ import { REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    {
+        name: 'roll',
+        description: 'Roll a dice',
+        options:[
+            {
+                name:"dice_expression",
+                description: "The dice expression in the format NdM (eg: 1d8)",
+                type: 3,
+                required: "true"
+            }
+        ]
+    },
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 try {
-  console.log('Started refreshing application (/) commands.');
+    console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands(process.env.APPLICATON_ID), { body: commands });
+    await rest.put(Routes.applicationCommands(process.env.APPLICATON_ID), { body: commands });
 
-  console.log('Successfully reloaded application (/) commands.');
+    console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
-  console.error(error);
+    console.error(error);
 }
